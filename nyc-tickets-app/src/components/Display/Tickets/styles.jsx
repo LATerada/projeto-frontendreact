3da987ccf21ed9b2ace563c501df5a9597871d3e
LATerada <larissaakiterada@gmail.com>
2023-02-09 10:22:37 -0300

    Syule update CardContainer min-width

--- nyc-tickets-app/src/components/Display/Tickets/styles.jsx
+++ nyc-tickets-app/src/components/Display/Tickets/styles.jsx
@@ -12,6 +12,7 @@ export const TicketsContainer = styled.section`
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `
 export const CardContainer = styled.div`
+    min-width: 298px;
     width: 48%;
     height: 30vh;
     position: relative;
